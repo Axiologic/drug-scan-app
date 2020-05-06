ca5ca6886288450d80927d88a2a4fc543b502d4c
Nicu-Cosmin Ursache <asac.cool@gmail.com>
2020-05-06 14:57:52 +0300

    wip

--- domain/index.js
+++ domain/index.js
@@ -1,1 +1,12 @@
 //Add specific code here (swarms, flows, assets, transactions)
+
+function createDummyLeafletDossier(productCode){
+	//here will be the logic to convert productCode into DSU Seed information
+	return "SEED_OF_DOSSIER";
+}
+
+$$.swarms.describe("eLeafLet", {
+	getLeafletDossier: function(productCode){
+		this.return(undefined, createDummyLeafletDossier(productCode));
+	}
+});
