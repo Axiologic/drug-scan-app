c2a6b256253c03e5b8aec59fc90e8a78f03d9486
skutner <sava.dumitru.daniel@gmail.com>
2020-05-29 08:47:37 +0300

    used DSUStorage instead of the History service

--- code/scripts/controllers/HistoryController.js
+++ code/scripts/controllers/HistoryController.js
@@ -2,41 +2,45 @@ import ContainerController from "../../cardinal/controllers/base-controllers/Con
 import History from "../services/History.js";
 
 export default class HistoryController extends ContainerController {
-	constructor(element, history) {
-		super(element);
-		console.log("Preparing to set up the view model");
-		let viewModel = {
-			clearHistoryBtn:{
-				label: "Clear History",
-				eventName: "clearHistory"
-			},
-			viewDrugBtn:{
-				label: ">",
-				eventName: "viewDrug"
-			}
-		}
-		this.model = this.setModel(viewModel);
-		let self = this;
+    constructor(element, history) {
+        super(element);
+        console.log("Preparing to set up the view model");
+        let viewModel = {
+            clearHistoryBtn: {
+                label: "Clear History",
+                eventName: "clearHistory"
+            },
+            viewDrugBtn: {
+                label: ">",
+                eventName: "viewDrug"
+            }
+        };
+        this.setModel(viewModel);
 
-		this.model.addExpression('historyLoaded', function () {
-			console.log("Expression checking", typeof self.model.scannedDrugs !== "undefined");
-			return typeof self.model.scannedDrugs !== "undefined";
-		}, 'scannedDrugs');
+        const self = this;
+        this.model.addExpression('historyLoaded', function() {
+            console.log("Expression checking", typeof self.model.scannedDrugs !== "undefined");
+            return typeof self.model.scannedDrugs !== "undefined";
+        }, 'scannedDrugs');
 
-		History.getTrack(function (err, track){
-			self.model.scannedDrugs = track;
-		});
+        this.DSUStorage.getItem('/data/drugsHistory.json', 'json', (err, drugHistory) => {
+            if(typeof drugHistory === "undefined"){
+                return this.model.scannedDrugs = [];
+            }
 
-		this.on("clearHistory", (event)=>{
-			History.clear(function(err, track){
-				if(!err){
-					self.model.scannedDrugs = [];
-				}
-			});
-		});
+            this.model.scannedDrugs = drugHistory;
+        });
 
-		this.on("view-drug", (event)=>{
-			history.push("/drug-details");
-		}, {capture: true});
-	}
+        this.on("clearHistory", (event) => {
+            this.DSUStorage.setItem('/data/drugsHistory.json', JSON.stringify([]), err => {
+                if (!err) {
+                    this.model.scannedDrugs = [];
+                }
+            });
+        });
+
+        this.on("view-drug", (event) => {
+            history.push("/drug-details");
+        }, {capture: true});
+    }
 }
--- code/scripts/models/Drug.js
+++ code/scripts/models/Drug.js
@@ -1,19 +1,19 @@
 function generateProductCode(length) {
-	var result           = '';
-	var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-	var charactersLength = characters.length;
-	for ( var i = 0; i < length; i++ ) {
-		result += characters.charAt(Math.floor(Math.random() * charactersLength));
-	}
-	return result;
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
 }
 
 export default class Drug {
-	productCode = generateProductCode(20);
-	commercialName = "Propoli PLUS";
-	validityDate = "02/08/2025";
-	recommendations = "Cough / Sore throat";
-	activeSubstances = "Propolis / Vitamin C";
-	contraIndications = "Headache";
-	dosage = "3 pills/day/4days";
+    productCode = generateProductCode(20);
+    commercialName = "Propolis PLUS";
+    validityDate = "02/08/2025";
+    recommendations = "Cough / Sore throat";
+    activeSubstances = "Propolis / Vitamin C";
+    contraIndications = "Headache";
+    dosage = "3 pills/day/4days";
 }
