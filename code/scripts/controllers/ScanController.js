2240a368f8f0c8c86f7984c7729faec04ebc6b44
skutner <sava.dumitru.daniel@gmail.com>
2020-08-25 09:34:24 +0300

    fixed createKeySSI method

--- code/scripts/controllers/ScanController.js
+++ code/scripts/controllers/ScanController.js
@@ -1,15 +1,14 @@
-import ContainerController from "../../cardinal/controllers/base-controllers/ContainerController.js";
-
+import ContainerController from '../../cardinal/controllers/base-controllers/ContainerController.js';
 export default class ScanController extends ContainerController {
 	constructor(element, history) {
 		super(element);
-		this.setModel({});
+		this.setModel({data: ''});
 
 		this.on("displayLeaflet", (event)=>{
 			history.push({
 				pathname: '/leaflet',
 				state: {
-					SGTIN: this.model.package_identifier
+					SGTIN: this.model.data
 				}
 			});
 		});
